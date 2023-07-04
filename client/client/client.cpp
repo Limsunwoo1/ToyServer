10e3f5f1041e6de039f76e5d5869f2a2fe5b505c
Limsunwoo1 <tjsdnhj@gmail.com>
2023-07-05 03:12:41 +0900

    구조 개선 귓속말 기능 추가

--- client/client/client.cpp
+++ client/client/client.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
 #include <thread>
-#include "def.h"
+#include "Engine.h"
 #include "CServerManager.h"
-
-void SendFunc()
-{
-	while (1)
-	{
-		GETSINGLE(ServerManager)->Send();
-	}
-}
 
 void ReceiveFunc()
 {
@@ -27,17 +19,50 @@ int main()
 	GETSINGLE(ServerManager)->ConvertIP();
 	GETSINGLE(ServerManager)->Connect();
 
-
-	std::thread sendthread(SendFunc);
 	std::thread receivethread(ReceiveFunc);
 
-	sendthread.detach();
 	receivethread.detach();
 
+
+	int a = 0;
 	while (1)
 	{
-		int a = 0;
+		
+
+		if (a >= 9999999)
+		{
+			ChatMassege_Packet chat = {};
+			chat.type = ServerDataType::ChatMessege;
+			chat.name = GETSINGLE(ServerManager)->GetClientName();
+
+			cout << "메세지를 입력하세요 : ";
+			char buf[256] = {};
+			chat.Messege = string(gets_s(buf));
+
+			if(chat.Messege.find("/") != string::npos)
+			{
+				int curNum = chat.Messege.find("/");
+				int endNum = chat.Messege.find(" ");
+
+				string name = chat.Messege.substr(curNum + 1, endNum - 1);
+				string messege = chat.Messege.substr(endNum + 1, chat.Messege.size());
+
+				WhisperMessege_Packet wPacket = {};
+				wPacket.type = ServerDataType::WhisperMessege;
+				wPacket.name = GETSINGLE(ServerManager)->GetClientName();
+				wPacket.Messege = messege;
+				wPacket.otherName = name;
+
+				GETSINGLE(ServerManager)->PushSend((void*)&wPacket);
+				continue;
+			}
+
+			GETSINGLE(ServerManager)->PushSend((void*)&chat);
+		}
+		a++;
 	}
 
+	// window Api 위에선 종료 시점에 호출 하면 된다.
+	GETSINGLE(ServerManager)->Rlease();
 	return 0;
 }
